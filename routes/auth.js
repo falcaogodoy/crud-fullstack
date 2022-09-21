b59f4dbfbd3ed4b3ce579ab487c0c728f530a5c1
francisco-godoy <falcaogodoy@gmail.com>
2022-09-21 14:45:04 -0300

    auth ok

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const validateRegisterInput = require("../validation/registerValidation");
+const jwt = require('jsonwebtoken');
+const requiresAuth = require('../middleware/permissions');
 
 
 
@@ -53,7 +55,7 @@ router.post("/register", async(req, res) =>{
         const userToReturn = { ...savedUser._doc };
         delete userToReturn.password;
 
-        // reorno novo user
+        //retorno novo user
         return res.json(userToReturn);
 
     } catch(err){
@@ -77,16 +79,48 @@ router.post("/login", async (req, res) =>{
         }
 
         const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
         if(!passwordMatch){
             return res.status(400).json({error: ' problema no seu login'});
         }
 
-        /* return res.json({passwordMatch: passwordMatch}); */
+        const payload = {userId: user._id};
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("access-token", token,{
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true, 
+            secure: process.env.NODE_ENV === "prodction"
+        } );
+
+        const userToReturn = { ...user._doc};
+        delete userToReturn.password;
+
+        return res.json({
+            token: token, 
+            user: userToReturn,
+        });
+
     }catch(err) {
         console.log(err);
         return res.status(500).send(err.message);
     }
 });
 
+
+//@route GET/api/auth/current
+//@desc Retorna usuario autenticado
+//@acess Private
+
+
+router.get("/current", requiresAuth, (req, res)=> {
+    if(!req.user) {
+        return res.status(401).send(" Nao Autorizado");
+    }
+
+    return res.json(req.user);
+});
 
 module.exports = router;
